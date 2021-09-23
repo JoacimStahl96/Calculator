e6d445a9b40c55594e6aadb9a2e3b32583e59954
Nikola Maljenovic <32510744+NikolaMC@users.noreply.github.com>
2021-09-23 11:49:34 +0200

    Exponents and parentheses work
    Made it so parentheses and exponents work in the input

--- src/main/java/CalculatorWithStrings.java
+++ src/main/java/CalculatorWithStrings.java
@@ -7,16 +7,16 @@ public class CalculatorWithStrings {
 
     public double mathValueFromStrings(String input) { // Converts the input value to a postfix string, using the Shunting-yard algorithm, passes the resulting postfix string to
                                                        // the getResultFromPostfix() method, which turns the postfix string into the result of the equation and returns that result
-
         if (input.isEmpty()) {
             throw new IllegalArgumentException("No value found");
         }
+
+        System.out.println("Input: " + input);
 
         String result = "";
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < input.length(); i++) { // Iterates over the input string and check each character. Does different things based on what that character is.
-                                                   // Parentheses don't seem to work currently.
 
             while (input.charAt(i) == ' ') { // If the character is a whitespace, skip it
                 ++i;
@@ -28,24 +28,22 @@ public class CalculatorWithStrings {
                 if (i + 1 >= input.length() || !Character.isDigit(input.charAt(i+1))) { // If the character after the digit is not a digit, add a whitespace to result instead
                     result += ' ';
                 }
+            } else if (input.charAt(i) == '(') { // If the character is a (, push it to the stack
+                stack.push(input.charAt(i));
+            } else if (input.charAt(i) == ')') { // If the character is a ), pop the last item from the stack and add it to result until a ( is encountered. When it is, remove it,
+                                                 // but don't add it to result
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    result += stack.pop();
+                }
+                stack.pop();
             } else if (getPrecedence(input.charAt(i)) != 0) { // If the current character is an operator
                 // While the stack isn't empty and the precedence of the last operator on the stack is greater than, or equal to the precedence of the current operator,
                 // add the last character from the stack to the result string, put a whitespace after it and remove it from the stack
                 while ((!stack.isEmpty()) && (getPrecedence(stack.peek()) >= getPrecedence(input.charAt(i))) && (stack.peek() != '(')) {
-                    result += stack.peek();
+                    result += stack.pop();
                     result += ' ';
-                    stack.pop();
                 }
                 stack.push(input.charAt(i)); // Add the current operator to the stack after removing the last operator from the stack
-            } else if (input.charAt(i) == '(') {
-                stack.push(input.charAt(i));
-            } else if (input.charAt(i) == ')') {
-                while (!stack.isEmpty() && stack.peek() != '(') {
-                    result += stack.peek();
-                    stack.pop();
-                }
-
-                stack.pop();
             }
         }
 
@@ -59,8 +57,8 @@ public class CalculatorWithStrings {
     }
 
     public double getResultFromPostfix(String postfix) { // Get the result from the postfix string
-        Stack<Integer> stack = new Stack<>();
-        System.out.println(postfix);
+        Stack<Double> stack = new Stack<>();
+        System.out.println("Parsed postfix string: " + postfix);
 
         for (int i = 0; i < postfix.length(); i++) {
             char c = postfix.charAt(i);
@@ -68,7 +66,7 @@ public class CalculatorWithStrings {
             if (c == ' ') { // Skip whitespaces
                 continue;
             } else if (Character.isDigit(c)) { // If the character is a digit, push it to the stack. If multiple digits are in a row, push them together
-                int n = 0;
+                double n = 0;
                 while(Character.isDigit(c)) {
                     n = n * 10 + (int)(c - '0');
                     i++;
@@ -82,26 +80,35 @@ public class CalculatorWithStrings {
                      // respectively, and adds them together. It then pushes the result, which is 3, to the stack. It keeps checking the following characters in the string and gets to 3.
                      // When it gets to the 2nd +, it stores the two 3s from the stack into val1 and val2 and adds them together, then pushes the result to the stack (6). Pushes 2 to the stack,
                      // gets to - and does the subtraction 6 - 2 and pushes 4 to the stack, which is the final result
-                int val1 = stack.pop();
-                int val2 = stack.pop();
+                double val1 = stack.pop();
+                double val2 = stack.pop();
+
+                if (val1 == 0) {
+                    throw new ArithmeticException("Division by zero");
+                }
 
                 switch (c) {
                     case '+':
-                        stack.push(val2 + val1);
+                        stack.push(addition(val2, val1));
                         break;
                     case '-':
-                        stack.push(val2 - val1);
+                        stack.push(subtraction(val2, val1));
                         break;
                     case '/':
-                        stack.push(val2 / val1);
+                        stack.push(division(val2, val1));
                         break;
                     case '*':
-                        stack.push(val2 * val1);
+                        stack.push(multiplication(val2, val1));
                         break;
+                    case '^':
+                        stack.push(Math.pow(val2, val1));
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Something went wrong");
                 }
             }
         }
-        System.out.println(stack);
+        System.out.println("Result: " + stack + "\n");
         return stack.pop(); // Return the result from the stack
     }
 
@@ -115,6 +122,9 @@ public class CalculatorWithStrings {
             case '*':
             case '/':
                 return 2;
+
+            case '^':
+                return 3;
         }
         return -1;
     }
@@ -147,21 +157,21 @@ public class CalculatorWithStrings {
         }
     }
 
-    public double simpleCalc(double num1, double num2, String type) {
-        switch (type) {
-            case "+" -> {
-                return addition(num1, num2);
-            }
-            case "-" -> {
-                return subtraction(num1, num2);
-            }
-            case "*" -> {
-                return multiplication(num1, num2);
-            }
-            case "/" -> {
-                return division(num1, num2);
-            }
-            default -> throw new IllegalArgumentException("Something went wrong");
-        }
-    }
+//    public double simpleCalc(double num1, double num2, String type) {
+//        switch (type) {
+//            case "+" -> {
+//                return addition(num1, num2);
+//            }
+//            case "-" -> {
+//                return subtraction(num1, num2);
+//            }
+//            case "*" -> {
+//                return multiplication(num1, num2);
+//            }
+//            case "/" -> {
+//                return division(num1, num2);
+//            }
+//            default -> throw new IllegalArgumentException("Something went wrong");
+//        }
+//    }
 }
