ca894f3f419ccd7f530ea8ebea0a6c8c7636bc89
Nikola Maljenovic <32510744+NikolaMC@users.noreply.github.com>
2021-09-24 16:13:14 +0200

    Refactoring
    Moved some code into separate functions

--- src/main/java/CalculatorWithStrings.java
+++ src/main/java/CalculatorWithStrings.java
@@ -19,39 +19,43 @@ public class CalculatorWithStrings {
                 ++i;
             }
 
-            if (Character.isDigit(input.charAt(i))) { // If the character is a digit, add it to the result String
-                result.append(input.charAt(i));
-
-                if (i + 1 >= input.length() || !Character.isDigit(input.charAt(i+1))) { // If the character after the digit is not a digit, add a whitespace to result instead
-                    result.append(' ');
-                }
-            } else if (input.charAt(i) == '(') { // If the character is a (, push it to the stack
-                stack.push(input.charAt(i));
-            } else if (input.charAt(i) == ')') { // If the character is a ), pop the last item from the stack and add it to result until a ( is encountered. When it is, remove it,
-                                                 // but don't add it to result
-                while (!stack.isEmpty() && stack.peek() != '(') {
-                    result.append(stack.pop());
-                    result.append(' ');
-                }
-                stack.pop();
-            } else if (getPrecedence(input.charAt(i)) != 0) { // If the current character is an operator
-                // While the stack isn't empty and the precedence of the last operator on the stack is greater than, or equal to the precedence of the current operator,
-                // add the last character from the stack to the result string, put a whitespace after it and remove it from the stack
-                while ((!stack.isEmpty()) && (getPrecedence(stack.peek()) >= getPrecedence(input.charAt(i))) && (stack.peek() != '(')) {
-                    result.append(stack.pop());
-                    result.append(' ');
-                }
-                stack.push(input.charAt(i)); // Add the current operator to the stack after removing the last operator from the stack
-            }
+            handleCharactersFromInput(input, result, stack, i);
         }
 
-        while (!stack.isEmpty()) { // Dump all of the remaining items from the stack into the result string
+        while (!stack.isEmpty()) { // Dump all the remaining items from the stack into the result string
             result.append(stack.pop());
             result.append(' ');
         }
 
         return getResultFromPostfix(result.toString()); // Return the result from the postfix string
 
+    }
+
+    private void handleCharactersFromInput(String input, StringBuilder result, Stack<Character> stack, int i) {
+        if (Character.isDigit(input.charAt(i))) { // If the character is a digit, add it to the result string
+            result.append(input.charAt(i));
+
+            if (i + 1 >= input.length() || !Character.isDigit(input.charAt(i +1))) { // If the character after the digit is not a digit, add a whitespace to result instead
+                result.append(' ');
+            }
+        } else if (input.charAt(i) == '(') { // If the character is a (, push it to the stack
+            stack.push(input.charAt(i));
+        } else if (input.charAt(i) == ')') { // If the character is a ), pop the last item from the stack and add it to result until a ( is encountered. When it is, remove it,
+                                             // but don't add it to result
+            while (!stack.isEmpty() && stack.peek() != '(') {
+                result.append(stack.pop());
+                result.append(' ');
+            }
+            stack.pop();
+        } else if (getPrecedence(input.charAt(i)) != 0) { // If the current character is an operator
+            // While the stack isn't empty and the precedence of the last operator on the stack is greater than, or equal to the precedence of the current operator,
+            // add the last character from the stack to the result string, put a whitespace after it and remove it from the stack
+            while ((!stack.isEmpty()) && (getPrecedence(stack.peek()) >= getPrecedence(input.charAt(i))) && (stack.peek() != '(')) {
+                result.append(stack.pop());
+                result.append(' ');
+            }
+            stack.push(input.charAt(i)); // Add the current operator to the stack after removing the last operator from the stack
+        }
     }
 
     public double getResultFromPostfix(String postfix) { // Get the result from the postfix string
@@ -62,38 +66,9 @@ public class CalculatorWithStrings {
             char c = postfix.charAt(i);
 
             if (Character.isDigit(c)) { // If the character is a digit, push it to the stack. If multiple digits are in a row, push them together
-                double n = 0;
-
-                while(Character.isDigit(c)) {
-                    n = n * 10 + (c - '0');
-                    i++;
-                    c = postfix.charAt(i);
-                }
-
-                i--;
-
-                stack.push(n);
+                i = pushDigitToStack(postfix, stack, i, c);
             } else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^') {
-                 // If the character is an operator, pop two numbers from the stack, store them in variables and apply that operator to them. Then push the result to the stack
-                 // For example: "1 + 2 + 3 - 2" gets converted to "1 2 + 3 + 2 -". 1 and 2 are pushed to the stack. When the loop gets to the 1st +, it stores 1 and 2 in val1 and val2
-                 // respectively, and adds them together. It then pushes the result, which is 3, to the stack. It keeps checking the following characters in the string and gets to 3.
-                 // When it gets to the 2nd +, it stores the two 3s from the stack into val1 and val2 and adds them together, then pushes the result to the stack (6). Pushes 2 to the stack,
-                 // gets to - and does the subtraction 6 - 2 and pushes 4 to the stack, which is the final result
-                double val1 = stack.pop();
-                double val2 = stack.pop();
-
-                if (c == '/' && val1 == 0) {
-                    throw new ArithmeticException("Division by zero");
-                }
-
-                switch (c) {
-                    case '+' -> stack.push(addition(val2, val1));
-                    case '-' -> stack.push(subtraction(val2, val1));
-                    case '/' -> stack.push(division(val2, val1));
-                    case '*' -> stack.push(multiplication(val2, val1));
-                    case '^' -> stack.push(Math.pow(val2, val1));
-                    default -> throw new IllegalArgumentException("Something went wrong");
-                }
+                doOperationsFromStack(stack, c);
             } else if (c != ' ') {
                 throw new IllegalArgumentException("Invalid character");
             }
@@ -102,6 +77,44 @@ public class CalculatorWithStrings {
         double result = stack.pop();
         System.out.printf("Result: %.1f%n%n", result);
         return Math.round(result); // Return the result from the stack
+    }
+
+    private void doOperationsFromStack(Stack<Double> stack, char c) {
+        // If the character is an operator, pop two numbers from the stack, store them in variables and apply that operator to them. Then push the result to the stack
+        // For example: "1 + 2 + 3 - 2" gets converted to "1 2 + 3 + 2 -". 1 and 2 are pushed to the stack. When the loop gets to the 1st +, it stores 1 and 2 in val1 and val2
+        // respectively, and adds them together. It then pushes the result, which is 3, to the stack. It keeps checking the following characters in the string and gets to 3.
+        // When it gets to the 2nd +, it stores the two 3s from the stack into val1 and val2 and adds them together, then pushes the result to the stack (6). Pushes 2 to the stack,
+        // gets to - and does the subtraction 6 - 2 and pushes 4 to the stack, which is the final result
+        double val1 = stack.pop();
+        double val2 = stack.pop();
+
+        if (c == '/' && val1 == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+
+        switch (c) {
+            case '+' -> stack.push(addition(val2, val1));
+            case '-' -> stack.push(subtraction(val2, val1));
+            case '/' -> stack.push(division(val2, val1));
+            case '*' -> stack.push(multiplication(val2, val1));
+            case '^' -> stack.push(Math.pow(val2, val1));
+            default -> throw new IllegalArgumentException("Something went wrong");
+        }
+    }
+
+    private int pushDigitToStack(String postfix, Stack<Double> stack, int i, char c) {
+        double n = 0;
+
+        while(Character.isDigit(c)) {
+            n = n * 10 + (c - '0');
+            i++;
+            c = postfix.charAt(i);
+        }
+
+        i--;
+
+        stack.push(n);
+        return i;
     }
 
     private int getPrecedence (char ch) { // Checks the precedence of the operators. * and / have the same precedence as each other and higher precedence than + and -, which also have
